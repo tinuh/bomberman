a1fc844cb7aa523df449ae40c62ad46021ec70e3
jonahw23 <109551453+jonahw23@users.noreply.github.com>
2022-12-13 14:15:56 -0500

    Added board and character w/ animation

--- src/main.js
+++ src/main.js
@@ -1,27 +1,107 @@
-import kaboom from "kaboom";
+import kaboom from "kaboom"
 
+// initialize context
 kaboom({
-	background: [51, 151, 255], // The RGB code
-});
+  background: [145, 137, 137], // The RGB code
+})
 
-loadSprite("bean", "sprites/bean.png");
+// load assets
+loadSprite("bean", "sprites/bean.png")
+loadSprite("stone", "sprites/StoneGround.png")
+loadSprite("player", 'sprites/character.png', {
+	sliceX: 4,
+	sliceY: 0,
+	anims: {
+		idle: { from: 0, to: 0 },
+		run: { from: 1, to: 3 }
+	}
+})
 
-const bean = add([pos(120, 80), sprite("bean"), area()]);
+let level1 = [
+  "===============",
+  "=             =",
+  "=             =",
+  "=             =",
+  "=             =",
+  "=             =",
+  "=             =",
+  "=             =",
+  "===============",
+]
 
-onClick(() => addKaboom(mousePos()));
+addLevel(level1, 
+         {width: 70,
+        height: 70,
+        "=": () => [
+          sprite("stone"),
+	        pos(80, 40),
+        	area(),
+          scale(2.3),
+          solid(),
+        ]}
+      )
+
+// add a character to screen
+const player = add([
+	// list of components
+	sprite("player", {
+			animSpeed: 0.1,
+		}),
+	pos(80, 40),
+	area(),
+  scale(3),
+  solid(),
+])
+let animationSpeed = 0
+
+function animateMove(){
+  //currently hardcoded
+  if(animationSpeed % 5 === 0){
+    if(player.frame < 3){
+    player.frame += 1
+    }
+    else{
+      player.frame = 1
+    }
+  }
+  animationSpeed += 1
+}
 
 onKeyDown("right", () => {
-	bean.move(100, 0);
-});
-
+    player.move(100, 0)
+    animateMove()
+})
 onKeyDown("left", () => {
-	bean.move(-100, 0);
-});
-
-onKeyDown("down", () => {
-	bean.move(0, 100);
-});
-
+    player.move(-100, 0)
+    animateMove()
+})
 onKeyDown("up", () => {
-	bean.move(0, -100);
-});
+    player.move(0, -100)
+    animateMove()
+})
+onKeyDown("down", () => {
+    player.move(0, 100)
+    animateMove()
+})
+onKeyRelease("right", () => {
+    player.play("idle")
+})
+onKeyRelease("left", () => {
+    player.play("idle")
+})
+onKeyRelease("up", () => {
+    player.play("idle")
+})
+onKeyRelease("down", () => {
+    player.play("idle")
+})
+
+
+// add a kaboom on mouse click
+onClick(() => {
+	addKaboom(mousePos())
+  player.moveTo(mousePos())
+})
+
+// burp on "b"
+onKeyPress("b", burp)
