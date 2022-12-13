95c2950331479cd49db8af695650ee114adbf10f
tinuh <tinuvanapamula@gmail.com>
2022-12-13 11:00:14 -0500

    basic stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-import kaboom from "kaboom"
+import kaboom from "kaboom";
 
-kaboom()
+kaboom({
+	background: [51, 151, 255], // The RGB code
+});
 
-loadSprite("bean", "sprites/bean.png")
+loadSprite("bean", "sprites/bean.png");
 
-add([
-	pos(120, 80),
-	sprite("bean"),
-])
+const bean = add([pos(120, 80), sprite("bean"), area()]);
 
-onClick(() => addKaboom(mousePos()))
+onClick(() => addKaboom(mousePos()));
+
+onKeyDown("right", () => {
+	bean.move(100, 0);
+});
+
+onKeyDown("left", () => {
+	bean.move(-100, 0);
+});
+
+onKeyDown("down", () => {
+	bean.move(0, 100);
+});
+
+onKeyDown("up", () => {
+	bean.move(0, -100);
+});
