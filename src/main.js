efc33caffbc7d1a3ac4a116bb5b19442dd01c839
jonahw23 <jonahwitte10@gmail.com>
2022-12-14 15:18:35 -0500

    Added bombs and explosions

--- src/main.js
+++ src/main.js
@@ -8,12 +8,22 @@ kaboom({
 // load assets
 loadSprite("bean", "sprites/bean.png")
 loadSprite("stone", "sprites/StoneGround.png")
-loadSprite("player", 'sprites/character.png', {
+loadSprite("bomb", "sprites/bomb.png")
+loadSprite("player", "sprites/character.png", {
 	sliceX: 4,
 	sliceY: 0,
 	anims: {
 		idle: { from: 0, to: 0 },
 		run: { from: 1, to: 3 }
+	}
+})
+
+loadSprite("boom", "sprites/explosion.png", {
+  sliceX: 0,
+  sliceY: 54,
+	anims: {
+		idle: { from: 0, to: 0 },
+		explode: { from: 1, to: 53 }
 	}
 })
 
@@ -47,13 +57,105 @@ const player = add([
 	sprite("player", {
 			animSpeed: 0.1,
 		}),
-	pos(160, 120),
+  "player",
+	pos(180, 140),
+  origin("center"),
 	area(),
   scale(3),
   solid(),
 ])
-let animationSpeed = 0
 
+function placeBomb(){
+  add([
+    // list of components
+    sprite("bomb", {
+        animSpeed: 5,
+      }),
+    pos(vec2(player.pos.x-3, player.pos.y+15)),
+    area(5),
+    scale(0.2),
+    area({ width: 120, height: 120 }),
+    origin(vec2(-0.2, 0.3)),
+    //solid(),
+    color(255, 0, 0),
+    "bomb",
+    {
+      fuse:0,
+    }
+  ])
+}
+
+function explode(location){
+  for(let i = 0; i < 4; i++){
+    add([
+      // list of components
+      sprite("boom", {
+          animSpeed: 5,
+        }),
+      "boom",
+      pos(location),
+      area({ width: 40, height: 20 }),
+      origin(vec2(-0.3, 0.1)),
+      scale(2),
+      color(255, 200, 0),
+      cleanup(),
+      {
+        angle: i*90,
+        exploding: false
+      }
+    ])
+  }
+}
+
+function pointAt(distance, angle) {
+  let radians = -1*deg2rad(angle);
+  return vec2(distance * Math.cos(radians), -distance * Math.sin(radians));
+}
+
+onUpdate("boom", (b) => {
+  if(!b.exploding){
+    b.play("explode")
+    b.exploding = true
+    b.time = 0;
+  }
+  else{
+    b.time += 1
+    if(b.time < 10){
+      b.move(pointAt(300, b.angle))
+    }
+    else if(b.time > 30 && b.time < 140){
+      b.scale.x = b.scale.x*0.9
+      b.scale.y = b.scale.y*0.9
+    }
+    else if(b.time > 140){
+      destroy(b)
+    }
+  }
+})
+
+let timer = 1
+onUpdate("bomb", (b) => {
+  console.log(b)
+  if(timer === 60){
+    timer = 1
+  }
+  timer += 1
+  if(timer % 13 === 0){
+    if(b.color.b === 255){
+      b.color = {r:255, b:0, g:0}
+    }
+    else{
+      b.color = {r:255, b:255, g:255}
+    }
+  }
+  b.fuse += 1
+  if(b.fuse > 80){
+    explode(b.pos)
+    destroy(b)
+  }
+})
+
+let animationSpeed = 0
 function animateMove(){
   //currently hardcoded
   if(animationSpeed % 5 === 0){
@@ -66,7 +168,9 @@ function animateMove(){
   }
   animationSpeed += 1
 }
-
+onKeyPress("space", () => {
+    placeBomb()
+})
 onKeyDown("right", () => {
     player.move(100, 0)
     animateMove()
@@ -101,6 +205,10 @@ onKeyRelease("down", () => {
 onClick(() => {
 	addKaboom(mousePos())
   player.moveTo(mousePos())
+})
+
+onCollide("boom", "player", () => {
+  console.log("death")
 })
 
 // burp on "b"
