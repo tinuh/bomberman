86169662e1cfb8a9657c9ec00bd037c1f8a0ebc7
Tinu Vanapamula <tinuvanapamula@gmail.com>
2022-12-16 15:18:15 +0000

    breakable walls

--- src/main.js
+++ src/main.js
@@ -31,40 +31,33 @@ const loadBg = async () => {
 const init = async () => {
 	await loadBg();
 
-// load assets
-loadSprite("bean", "sprites/bean.png")
-loadSprite("stone", "sprites/StoneGround.png")
-loadSprite("bomb", "sprites/bomb.png")
-loadSprite("player", 'sprites/character.png', {
-	sliceX: 4,
-	sliceY: 0,
-	anims: {
-		idle: { from: 0, to: 0 },
-		run: { from: 1, to: 3 }
-	}
-})
-loadSprite("enemy", "sprites/floateye.png", {
-  sliceX: 3,
-  sliceY: 3,
-  anims: {
-    idle: { from: 0, to: 2 },
-    run: { from: 3, to: 5 },
-  },
-})
-loadSprite("boom", "sprites/explosion.png", {
-  sliceX: 0,
-  sliceY: 54,
-	anims: {
-		idle: { from: 0, to: 0 },
-		explode: { from: 1, to: 53 }
-	}
-})
+	// load assets
+	loadSprite("bean", "sprites/bean.png");
+	loadSprite("stone", "sprites/StoneGround.png");
+	loadSprite("bomb", "sprites/bomb.png");
+	loadSprite("player", "sprites/character.png", {
+		sliceX: 4,
+		sliceY: 0,
+		anims: {
+			idle: { from: 0, to: 0 },
+			run: { from: 1, to: 3 },
+		},
+	});
+	loadSprite("boom", "sprites/explosion.png", {
+		sliceX: 0,
+		sliceY: 54,
+		anims: {
+			idle: { from: 0, to: 0 },
+			explode: { from: 1, to: 53 },
+		},
+	});
+  loadSprite("breakableStone", "sprites/brick1.png");
 
 	let level1 = [
-		"===============",
+		"====^==========",
 		"=  =       =  =",
-		"=    =        =",
-		"===       =   =",
+		"=    ^        =",
+		"===       ^   =",
 		"=      =      =",
 		"=  =       =  =",
 		"=      =     ==",
@@ -76,171 +69,170 @@ loadSprite("boom", "sprites/explosion.png", {
 		width: 70,
 		height: 70,
 		"=": () => [sprite("stone"), pos(80, 40), area(), scale(2.3), solid()],
-	  })
+    "^": () => [sprite("breakableStone"), "breakableStone", pos(80, 40), area(), scale(0.16, 0.17), solid()],
+	});
 
-// add a character to screen
-const player = add([
-	// list of components
-	sprite("player", {
+	// add a character to screen
+	const player = add([
+		// list of components
+		sprite("player", {
 			animSpeed: 0.1,
 		}),
-  "player",
-	pos(180, 140),
-  origin("center"),
-	area(),
-  scale(3),
-  solid(),
-])
+		"player",
+		pos(180, 140),
+		origin("center"),
+		area(),
+		scale(3),
+		solid(),
+	]);
 
-function placeBomb(){
-  add([
-    // list of components
-    sprite("bomb", {
-        animSpeed: 5,
-      }),
-    pos(vec2(player.pos.x-3, player.pos.y+15)),
-    area(5),
-    scale(0.2),
-    area({ width: 120, height: 120 }),
-    origin(vec2(-0.2, 0.3)),
-    //solid(),
-    color(255, 0, 0),
-    "bomb",
-    {
-      fuse:0,
-    }
-  ])
-}
+	function placeBomb() {
+		add([
+			// list of components
+			sprite("bomb", {
+				animSpeed: 5,
+			}),
+			pos(vec2(player.pos.x - 3, player.pos.y + 15)),
+			area(5),
+			scale(0.2),
+			area({ width: 120, height: 120 }),
+			origin(vec2(-0.2, 0.3)),
+			//solid(),
+			color(255, 0, 0),
+			"bomb",
+			{
+				fuse: 0,
+			},
+		]);
+	}
 
-function explode(location){
-  for(let i = 0; i < 4; i++){
-    add([
-      // list of components
-      sprite("boom", {
-          animSpeed: 5,
-        }),
-      "boom",
-      pos(location.add(Math.sin(i*90)*-10,-10+Math.cos(i*90)*10)),
-      area(i%2 === 0 ? 
-        { width: 40, height: 10 } : 
-        { width: 10, height: 40 } ),
-      scale(2),
-      origin(vec2(-0.3,0.1)),
-      color(255, 200, 0),
-      cleanup(),
-      {
-        angle: i*90,
-        exploding: false
-      }
-    ])
-  }
-}
+	function explode(location) {
+		for (let i = 0; i < 4; i++) {
+			add([
+				// list of components
+				sprite("boom", {
+					animSpeed: 5,
+				}),
+				"boom",
+				pos(location.add(Math.sin(i * 90) * -10, -10 + Math.cos(i * 90) * 10)),
+				area(
+					i % 2 === 0 ? { width: 40, height: 10 } : { width: 10, height: 40 }
+				),
+				scale(2),
+				origin(vec2(-0.3, 0.1)),
+				color(255, 200, 0),
+				cleanup(),
+				{
+					angle: i * 90,
+					exploding: false,
+				},
+			]);
+		}
+	}
 
-function pointAt(distance, angle) {
-  let radians = -1*deg2rad(angle);
-  return vec2(distance * Math.cos(radians), -distance * Math.sin(radians));
-}
+	function pointAt(distance, angle) {
+		let radians = -1 * deg2rad(angle);
+		return vec2(distance * Math.cos(radians), -distance * Math.sin(radians));
+	}
 
-onCollide("player", "boom", () => {
-  console.log("death")
-})
+	onCollide("player", "boom", () => {
+		console.log("death");
+	});
 
-onUpdate("boom", (b) => {
-  if(!b.exploding){
-    b.play("explode")
-    if(b.angle === 180){
-      b.pos.x -= 10
-      b.area.offset.x -= 20
-      b.area.offset.y += 10
-    }
-    if(b.angle === 90){
-      b.area.offset.y += 20
-    }
-    b.exploding = true
-    b.time = 0;
-  }
-  else{
-    b.time += 1
-    if(b.time < 10){
-      b.move(pointAt(300, b.angle))
-    }
-    else if(b.time > 30 && b.time < 80){
-      b.scale.x = b.scale.x*0.9
-      b.scale.y = b.scale.y*0.9
-    }
-    else if(b.time > 80){
-      destroy(b)
-    }
-  }
-})
+	onCollide("boom", "breakableStone", (boom, breakableStone) => {
+		destroy(breakableStone);
+	});
 
-let timer = 1
-onUpdate("bomb", (b) => {
-  if(timer === 60){
-    timer = 1
-  }
-  timer += 1
-  if(timer % 13 === 0){
-    if(b.color.b === 255){
-      b.color = {r:255, b:0, g:0}
-    }
-    else{
-      b.color = {r:255, b:255, g:255}
-    }
-  }
-  b.fuse += 1
-  if(b.fuse > 80){
-    explode(b.pos)
-    destroy(b)
-  }
-})
+	onUpdate("boom", (b) => {
+		if (!b.exploding) {
+			b.play("explode");
+			if (b.angle === 180) {
+				b.pos.x -= 10;
+				b.area.offset.x -= 20;
+				b.area.offset.y += 10;
+			}
+			if (b.angle === 90) {
+				b.area.offset.y += 20;
+			}
+			b.exploding = true;
+			b.time = 0;
+		} else {
+			b.time += 1;
+			if (b.time < 10) {
+				b.move(pointAt(300, b.angle));
+			} else if (b.time > 30 && b.time < 80) {
+				b.scale.x = b.scale.x * 0.9;
+				b.scale.y = b.scale.y * 0.9;
+			} else if (b.time > 80) {
+				destroy(b);
+			}
+		}
+	});
 
-let animationSpeed = 0
+	let timer = 1;
+	onUpdate("bomb", (b) => {
+		if (timer === 60) {
+			timer = 1;
+		}
+		timer += 1;
+		if (timer % 13 === 0) {
+			if (b.color.b === 255) {
+				b.color = { r: 255, b: 0, g: 0 };
+			} else {
+				b.color = { r: 255, b: 255, g: 255 };
+			}
+		}
+		b.fuse += 1;
+		if (b.fuse > 80) {
+			explode(b.pos);
+			destroy(b);
+		}
+	});
 
-function animateMove(){
-  //currently hardcoded
-  if(animationSpeed % 5 === 0){
-    if(player.frame < 3){
-    player.frame += 1
-    }
-    else{
-      player.frame = 1
-    }
-  }
-  animationSpeed += 1
-}
+	let animationSpeed = 0;
 
-onKeyPress("space", () => {
-  placeBomb()
-})
-onKeyDown("right", () => {
-    player.move(100, 0)
-    animateMove()
-})
-onKeyDown("left", () => {
-    player.move(-100, 0)
-    animateMove()
-})
-onKeyDown("up", () => {
-    player.move(0, -100)
-    animateMove()
-})
-onKeyDown("down", () => {
-    player.move(0, 100)
-    animateMove()
-})
-onKeyRelease("right", () => {
-    player.play("idle")
-})
-onKeyRelease("left", () => {
-    player.play("idle")
-})
-onKeyRelease("up", () => {
-    player.play("idle")
-})
-onKeyRelease("down", () => {
-    player.play("idle")
-})
+	function animateMove() {
+		//currently hardcoded
+		if (animationSpeed % 5 === 0) {
+			if (player.frame < 3) {
+				player.frame += 1;
+			} else {
+				player.frame = 1;
+			}
+		}
+		animationSpeed += 1;
+	}
 
+	onKeyPress("space", () => {
+		placeBomb();
+	});
+	onKeyDown("right", () => {
+		player.move(100, 0);
+		animateMove();
+	});
+	onKeyDown("left", () => {
+		player.move(-100, 0);
+		animateMove();
+	});
+	onKeyDown("up", () => {
+		player.move(0, -100);
+		animateMove();
+	});
+	onKeyDown("down", () => {
+		player.move(0, 100);
+		animateMove();
+	});
+	onKeyRelease("right", () => {
+		player.play("idle");
+	});
+	onKeyRelease("left", () => {
+		player.play("idle");
+	});
+	onKeyRelease("up", () => {
+		player.play("idle");
+	});
+	onKeyRelease("down", () => {
+		player.play("idle");
+	});
 };
-init()
+init();
