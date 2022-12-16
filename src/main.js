fe50ba21ed46dffdd92067fe28f7de31d830dec6
jonahw23 <jonahwitte10@gmail.com>
2022-12-16 10:17:55 -0500

    Slime enemy

--- src/main.js
+++ src/main.js
@@ -51,6 +51,14 @@ loadSprite("enemy", "sprites/floateye.png", {
     run: { from: 3, to: 5 },
   },
 })
+loadSprite("slime", "sprites/slime.png", {
+  sliceX: 7,
+  sliceY: 5,
+  anims: {
+    idle: { from: 0, to: 4 },
+    death: { from: 28, to: 32 },
+  },
+})
 loadSprite("boom", "sprites/explosion.png", {
   sliceX: 0,
   sliceY: 54,
@@ -92,6 +100,25 @@ const player = add([
   solid(),
 ])
 
+const slime = add([
+  sprite("slime", {
+    animSpeed: 1,
+  }),
+"enemy",
+"slime",
+"pain",
+pos(180, 180),
+origin("center"),
+area({ width: 15, height: 12 }),
+origin(vec2(0, 0.25)),
+scale(3),
+solid(),
+{
+  moveDir: RIGHT,
+  dead: false,
+}
+])
+
 function placeBomb(){
   add([
     // list of components
@@ -120,6 +147,7 @@ function explode(location){
           animSpeed: 5,
         }),
       "boom",
+      "pain",
       pos(location.add(Math.sin(i*90)*-10,-10+Math.cos(i*90)*10)),
       area(i%2 === 0 ? 
         { width: 40, height: 10 } : 
@@ -143,6 +171,12 @@ function pointAt(distance, angle) {
 
 onCollide("player", "boom", () => {
   console.log("death")
+})
+
+onCollide("slime", "boom", (slime) => {
+  slime.dead = true
+  slime.play("death")
+  wait(0.8, () => {destroy(slime)})
 })
 
 onUpdate("boom", (b) => {
@@ -174,6 +208,7 @@ onUpdate("boom", (b) => {
   }
 })
 
+
 let timer = 1
 onUpdate("bomb", (b) => {
   if(timer === 60){
@@ -195,39 +230,51 @@ onUpdate("bomb", (b) => {
   }
 })
 
-let animationSpeed = 0
+onUpdate("slime", (s) => {
+  if(!s.dead){
+    animateMove(s, 7, 4)
+    s.move(s.moveDir.scale(40))
+    if(Math.random() < 0.005){
+      dirs = [RIGHT, LEFT, UP, DOWN]
+      s.moveDir = dirs[Math.floor(Math.random() * dirs.length)]
+  }
+  }
+})
 
-function animateMove(){
+let animationCount = 0
+
+function animateMove(obj, animationSpeed, max){
   //currently hardcoded
-  if(animationSpeed % 5 === 0){
-    if(player.frame < 3){
-    player.frame += 1
+  if(animationCount % animationSpeed === 0){
+    if(obj.frame < max - 1){
+    obj.frame += 1
     }
     else{
-      player.frame = 1
+      obj.frame = 0
     }
   }
-  animationSpeed += 1
+  animationCount += 1
 }
 
 onKeyPress("space", () => {
   placeBomb()
+  slime.play("idle")
 })
 onKeyDown("right", () => {
     player.move(100, 0)
-    animateMove()
+    animateMove(player, 5, 4)
 })
 onKeyDown("left", () => {
     player.move(-100, 0)
-    animateMove()
+    animateMove(player, 5, 4)
 })
 onKeyDown("up", () => {
     player.move(0, -100)
-    animateMove()
+    animateMove(player, 5, 4)
 })
 onKeyDown("down", () => {
     player.move(0, 100)
-    animateMove()
+    animateMove(player, 5, 4)
 })
 onKeyRelease("right", () => {
     player.play("idle")
